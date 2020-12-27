c5d6382379e3976a8f7c9ead3dcb9b0aece89bc8
luisblafer <32635768+luisblafer@users.noreply.github.com>
2020-12-27 16:44:47 -0300

    Create DSF_TP0_S32_ODE45.m

---
+++ DSF_TP0_S32_ODE45.m
@@ -0,0 +1,109 @@
+%% GRAFICAS DE SOLUCIONES PARA LA POSICION Y VELOCIDAD DEL PENDULO
+% SE GRAFICAN LA SOLUCION ANALITICA vs. LAS SIMULACIONES EN SIMULINK DEL
+% MODELO SIMPLIFICADO, EMPLEANDO EN METODO DE DORMAND-PRINCE
+% PASOS DE INTEGRACION:
+%
+%           h = [0.1]
+%
+% ADICIONALMENTE SE CALCULA LA CANTIDAD DE PASOS DE INTEGRACION QUE LE
+% INSUME A CADA UNO.
+
+
+init
+
+printEps = true;
+
+
+% Atributos generales
+h = [0.1];
+t0 = 0;
+tf = 50;
+tspan = [t0 tf];
+y0 = [1 0]';
+
+
+% Atributos para identificar a los Archivos/Graficas.
+FileHead = 'PLOT_32';
+
+
+% EJECUTAMOS EL SCRIPT QUE SE OCUPA DE GENERAR UNA SOLUCION ANALITICA DEL
+% SISTEMA y TRAER LAS VARIABLES AL ESPACIO DE TRABAJO
+% 
+%    VARIABLES DE SALIDA: S.Analitica.t
+%                         S.Analitica.x1
+%                         S.Analitica.x2                    
+%
+
+DSF_TP0_S3E_COMMON_ANALITICA;
+
+
+% EJECUTAMOS EL SCRIPT QUE SE OCUPA DE SIMULAR CON TODOS LOS METODOS Y
+% ATRIBUTOS PEDIDOS PARA EL SISTEMA, Y DE TRAER AL ESPACIO DE TRABAJO LOS
+% RESULTADOS EN UNA ESTRUCTURA
+% 
+%    CONJUNTO DE SALIDAS:   S.Euler.*
+%                           S.Heun.*
+%                           S.DormandPrince.*
+%                           S.DormandPrinceDecimado.*
+%                           S.NDF.*
+%
+
+DSF_TP0_S3E_COMMON_PENDULO;
+
+
+
+%%
+% GRAFICAS ANALITICA vs. SIMULACION EN SIMULINK DE LA POSICION, CON EL
+% METODOS DE DORMAND-PRINCE (ode45)
+
+t = {S.Analitica.t, S.DormandPrince.t};
+x = {S.Analitica.x1, S.DormandPrince.x1};
+
+optionsNameValStruct.FigureName     = [FileHead '_ODE45_Posicion'];
+optionsNameValStruct.LegendText     = {'Analitica', 'Metodo Dormand-Prince'};
+optionsNameValStruct.LineColor      = [[0.1 0.1 0.1]; [0 0.477 0.7410]];
+optionsNameValStruct.LineWidth      = [1,2];
+optionsNameValStruct.XLabelText     = 'Tiempo [s]';
+optionsNameValStruct.YLabelText     = '$\theta(t)$';
+optionsNameValStruct.Interpreter    = 'latex';
+optionsNameValStruct.XLimSpan       = [0 50];
+optionsNameValStruct.YLimSpan       = [-1 1];
+optionsNameValStruct.LegendLocation = 'northeast';
+
+
+plotAutomatico(t,x,optionsNameValStruct)
+
+
+% IMPRIMIR IMAGEN A .EPS
+if printEps
+    print(['images/' optionsNameValStruct.FigureName], '-depsc');
+end
+
+
+%%
+% GRAFICAS ANALITICA vs. SIMULACION EN SIMULINK DE LA VELOCIDAD, CON EL
+% METODO DE DORMAND-PRINCE (ode45)
+
+t = {S.Analitica.t, S.DormandPrince.t};
+x = {S.Analitica.x2, S.DormandPrince.x2};
+
+optionsNameValStruct.FigureName     = [FileHead '_ODE45_Velocidad'];
+optionsNameValStruct.LegendText     = {'Analitica', 'Metodo Dormand-Prince'};
+optionsNameValStruct.LineColor      = [[0.1 0.1 0.1]; [0 0.477 0.7410]];
+optionsNameValStruct.LineWidth      = [1,2];
+optionsNameValStruct.XLabelText     = 'Tiempo [s]';
+optionsNameValStruct.YLabelText     = '$\omega(t)$';
+optionsNameValStruct.Interpreter    = 'latex';
+optionsNameValStruct.XLimSpan       = [0 50];
+optionsNameValStruct.YLimSpan       = [-1 1];
+optionsNameValStruct.LegendLocation = 'northeast';
+
+
+plotAutomatico(t,x,optionsNameValStruct)
+
+
+% IMPRIMIR IMAGEN A .EPS
+if printEps
+    print(['images/' optionsNameValStruct.FigureName], '-depsc');
+end
+

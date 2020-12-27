6372f123823b42416ce2aa691b418af906324f1c
luisblafer <32635768+luisblafer@users.noreply.github.com>
2020-12-27 15:54:43 -0300

    Create init.m
    Main init package to add paths

---
+++ init.m
@@ -0,0 +1,13 @@
+%% INICIALIZACION DEL AMBIENTE DE MATLAB
+% SE AGREGAN AL AMBIENTE DIRECTORIOS QUE CONTIENEN FUNCIONES Y SCRIPTS
+% QUE SON EMPLEADOS POR LOS DIFERENTES SCRIPTS PRINCIPALES.
+
+% Cleaning
+clear all
+close all
+clc
+
+% Add paths
+addpath('scripts')
+addpath('sfun')
+addpath('cfun')
